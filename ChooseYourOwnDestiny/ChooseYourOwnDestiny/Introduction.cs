dfb5dfd8d5184a53a562c0b875aaf873346f0743
Jabbrowocky <mindbull3tz@gmail.com>
2017-11-16 19:25:01 -0600

    sucessfully implemented a fork in program, including a dice roll function and some more logic

--- ChooseYourOwnDestiny/ChooseYourOwnDestiny/Introduction.cs
+++ ChooseYourOwnDestiny/ChooseYourOwnDestiny/Introduction.cs
@@ -8,24 +8,63 @@ namespace ChooseYourOwnDestiny
 {
     class Introduction
     {
-        public bool pathA;
-        public bool pathB;
+        private bool pathA;
+        private bool pathB;
+        private string response;
+
         public Introduction(bool pathA, bool pathB)
         {
             this.pathA = pathA;
             this.pathB = pathB;
-            DisplayIntroduction();
+            
+            StoryArcer(DisplayIntroduction());
+            
         }
 
-        public void DisplayIntroduction()
+        public string DisplayIntroduction()
         {
+            
             Console.WriteLine("The Day devCodeCamp Stood Still \n");
             Console.ReadKey();
             Console.WriteLine("A Choose Your Own Destiny Adventure by Erik White\n\n");
             Console.ReadKey();
-            Console.WriteLine(" It is a day, like any other at the Camp de la DevCode. You arrive at the Pritzlaff building and head \nto the elevator, weary from a heavy night of drinking and trying to forget how much you learned the previous day.\nYou slept poorly, tossing and turning, waking up several times, and now you have to cram a month's worth\nof information into several hours. However, as you approach the classroom door, something feels off, the usual\nmirth of the early risers you've gotten to know as Team Cobalt is non-existant, replaced by fear, panic\nand a grim sadness.  On the floor lies Mike, placed almost whimsically on his lifeless chest is a single\nPikachu Pokemon card with a symbol, recognizable as an octathope, written on it in sharpie .");
-            Console.ReadKey();
+            Console.WriteLine("  It is a day, like any other at the Camp de la DevCode. You arrive at the Pritzlaff building and head \nto the elevator, weary from a heavy night of drinking and trying to forget how much you learned the previous day.\nYou slept poorly, tossing and turning, waking up several times, and now you have to cram a month's worth\nof information into several hours. However, as you approach the classroom door, something feels off, the usual\nmirth of the early risers you've gotten to know as Team Cobalt is non-existant, replaced by fear, panic\nand a grim sadness.  On the floor lies Mike, placed almost whimsically on his lifeless chest is a single\nPikachu Pokemon card with a symbol, recognizable as an octathope, written on it in sharpie.\nHow do you react?\n");
+            Console.WriteLine("  (Option 1) Coward: Run like hell and call the police");
+            Console.WriteLine("  (Option 2) Gumshoe: Investigate the scene of the crime\n");
+            Console.WriteLine("  So, what's it gonna be? 'coward' or 'gumshoe'?");
+            response = Console.ReadLine().ToLower();
+            return response;
             
+
+            //Random DiceRoll = new Random();
+            //Console.WriteLine(DiceRoll.Next(1, 7));
+            //Console.ReadKey();
+
+        }
+        public void StoryArcer(string introCapture)
+        {
+            switch (introCapture)
+            {
+                case "coward":
+                    pathA = true;
+                    break;
+                case "gumshoe":
+                    pathB = true;
+                    break;
+                default:
+                    Console.WriteLine("So, what's it gonna be? 'coward' or 'gumshoe'?");
+                    introCapture = Console.ReadLine();
+                    StoryArcer(introCapture);
+                    break;
+            }
+            if (pathA == true)
+            {
+                coward cowardPath = new coward();
+            }
+            else if (pathB == true)
+            {
+                gumshoe gumshoePath = new gumshoe();
+            }
         }
     }
 }
---
+++ ChooseYourOwnDestiny/ChooseYourOwnDestiny/coward.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChooseYourOwnDestiny
+{
+    class coward
+    {
+        string response;
+        public coward()
+        {
+            CowardEndingLeadUp();
+        }
+        private void CowardEndingLeadUp()
+        {
+            Console.WriteLine("In a panic you run out of the room. Mike was easily your favorite instructor *cough* ANDREW *cough*, and there he was, lying on the floor dead! Your head is throbbing with pain from last night's festivities and this is what you walk into, and the sun hasn't even crested the horizon fully. You call the police, and wait in the bathroom, having a panic attack, unable to come face to face with the gravity of the situation. \nThe police arrive at the scene and begin to intervew anyone who might have witnessed anything, yourself included\n\n");
+            Console.WriteLine("The Police officer approaches you, you look at him nervously, you were never a troublemaker (yeah right), but at least you know you're not guilty./n The Officer says 'Hi, I'm Officer Larry Digby of the Milwaukee police department, may I ask you a few questions?\n");
+            Console.WriteLine(" (Option 1) RUN: You opt to book it, not this time copper! (3/20 to escape)");
+            Console.WriteLine(" (Option 2) Chat: Engage in dialogue with Officer Digby");
+            Console.WriteLine("So are you going to flee in 'cowardice', again, or 'chat' with the nice officer");
+            response = Console.ReadLine().ToLower();
+            Console.ReadKey();
+            CowardArc(response);
+
+        }
+        private void OfficerConversation()
+        {
+
+        }
+        private void CowardArc(string response)
+        {
+            
+            switch (response)
+            {
+                case "cowardice":
+                    CowardLogic(CowardRoll());
+                    break;
+                case "chat":
+                    break;
+                default:
+                    Console.WriteLine("That is an invalid response, please enter either 'cowardice' or 'chat'.");
+                        CowardArc(response);
+                    break;
+            }
+        }
+        private int CowardRoll()
+        {
+            int rollCoward;
+            Random rollDice = new Random();
+            rollCoward = rollDice.Next(1, 21);
+            return rollCoward;
+        }
+        private void CowardLogic(int diceRoll)
+        {
+            if (diceRoll % 6 == 0)
+            {
+                Console.WriteLine("  You successfully outrun the police whilst dodging hails of gun fire and drive off into the sunset; you win?");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("  Turns out you should have stuck to coding and not tried to be an athlete, don't run from the police, stupid.  You lose.");
+                Console.ReadKey();
+            }
+        }
+
+    }
+}
